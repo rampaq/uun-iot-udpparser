6b39689f8496f4f6f0a25b7411f5a84a685b58f7
Karel Blavka <karel.blavka@hardwario.com>
2023-11-20 23:06:58 +0100

    Add data forwarding via http

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # UDP Server ith CBOR Decoder
 
+Data forwarding via the HTTP endpoint is activated using the `HTTP_URL` variable. Example: HTTP_URL="https://best.app.example"
+
+Forwarding via MQTT with `MQTT_ENABLE=true`
 ## Environ
 
 | Name | Default |
@@ -8,6 +11,8 @@
 | DECODER_YAML | ./decoder.yaml |
 | LOG_LEVEL | info
 | LOG_PRETTY | false
+| HTTP_URL | |
+| MQTT_ENABLE | false
 | MQTT_BROKER_URL | mqtt://localhost:1883
 | MQTT_USERNAME | null |
 | MQTT_PASSWORD | null |
@@ -15,7 +20,6 @@
 | MQTT_CA | null |
 | MQTT_CERT | null |
 | MQTT_KEY | null |
-
 
 ## Local dev
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 const fs = require('fs');
 const log = require('./log');
+const axios = require('axios');
 const UDPServer = require('./UDPServer');
 const MQTTClient = require('./MQTTClient');
 const { getDecoderFromFile } = require('./decoder');
@@ -35,7 +36,27 @@ const decoder = getDecoderFromFile(decoderYaml);
 
 log.info('Connecting to MQTT broker: %s', mqttUrl);
 
-const mqttclient = new MQTTClient(mqttUrl, mqttOptions);
+const mqttclient = process.env.MQTT_ENABLE ? new MQTTClient(mqttUrl, mqttOptions) : null;
+const httpUrl = process.env.HTTP_URL || null;
+
+async function publish(msg) {
+  if (mqttclient) {
+    const payload = JSON.stringify(msg, (key, value) => (typeof value === 'bigint'
+      ? value.toString()
+      : value));
+    mqttclient.publish(`chester/${msg.serial_number}`, payload);
+  }
+
+  if (httpUrl) {
+    axios.post(process.env.HTTP_URL, msg, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).catch((err) => {
+      log.error(err);
+    });
+  }
+}
 
 const udpserver = new UDPServer();
 
@@ -44,11 +65,7 @@ udpserver.on('message', (msg) => {
   msg.data = decoder.decode(msg.raw);
   msg.raw = msg.raw.toString('hex');
 
-  const payload = JSON.stringify(msg, (key, value) => (typeof value === 'bigint'
-    ? value.toString()
-    : value));
-
-  mqttclient.publish(`chester/${msg.serial_number}`, payload);
+  publish(msg);
 });
 
 udpserver.listen(process.env.PORT || 5000);
